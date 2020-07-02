7a73df88f4d265e1bc3c496bea008a6097a4bc3b
yossy0201 <yossy0201msk@gmail.com>
2020-07-02 22:01:30 +0900

    space

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 server '<用意したElastic IP>', user: 'ec2-user', roles: %w{app db web}
 
 
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
